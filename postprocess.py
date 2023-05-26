4bc002a44c20ffdb949a04c191a6b42bafe52314
chenchenhou <cchou90@gmail.com>
2023-05-26 16:34:53 +0800

    update inference.py

--- postprocess.py
+++ postprocess.py
@@ -4,10 +4,8 @@ import numpy as np
 
 
 def draw_segmentation_map(pred: np.ndarray, label_map: dict) -> np.ndarray:
-    # labels = torch.argmax(outputs.squeeze().cpu(), dim=0).numpy()
     # Create 3 Numpy arrays containing zeros.
-    # Later each pixel will be filled with respective red, green, and blue pixels
-    # depending on the predicted class.
+    # Later each pixel will be filled with respective red, green, and blue pixels depending on the predicted class.
 
     R_map = np.zeros_like(pred).astype(np.uint8)
     G_map = np.zeros_like(pred).astype(np.uint8)
@@ -29,12 +27,14 @@ def connected_components(pred: np.ndarray, threshold: int) -> np.ndarray:
     max_area, max_label = 0, -1
     background_color = 0
 
+    # Find the region with largest area (area has to be larget that threshold)
     for label in range(1, num_labels):
         area = stats[label, cv2.CC_STAT_AREA]
         if (area > max_area) and (area > threshold):
             max_area = area
             max_label = label
 
+    # Replace other regions with background color
     labels = labels.astype(np.uint8)
     labels[labels != max_label] = background_color
     cleaned_pred = labels
@@ -48,14 +48,14 @@ def is_ellipse(contour, threshold=0.55):
         return False
     ellipse = cv2.fitEllipse(contour)
     _, (axes), _ = ellipse
-
     minor_axis, major_axis = min(axes), max(axes)
+
     # Define threshold for eccentricity
     eccentricity_threshold = threshold
 
     # Calculate eccentricity
     eccentricity = minor_axis / major_axis
-    print(eccentricity)
+
     # Check if eccentricity suggests an ellipse
     if eccentricity > eccentricity_threshold:
         return True
@@ -67,6 +67,7 @@ def find_pupil(mask):
     edges = cv2.Canny(mask, 30, 200)
     contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     not_pupil_masks = []
+
     for m in contours:
         candidate = np.squeeze(m)
         result = is_ellipse(candidate)
